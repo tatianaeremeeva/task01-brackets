63d7426ffda509268349be8dd48f8c91900269ba
tatianaeremeeva <tatianaeremeeva667@gmail.com>
2021-07-14 16:51:12 +0300

    Арифметическое выражение

---
+++ test_check.php
@@ -0,0 +1,21 @@
+﻿<HTML>
+<HEAD>
+   <TITLE> Основное задание </TITLE>
+</HEAD>
+<BODY>
+<?php
+$input= $_GET['input'];
+echo "Вы ввели ".$input;
+$counterRight=0;$counterLeft=0;
+for($i=0;$i<strlen($input);$i++)
+{	
+	if($input[$i]==')')	{$counterLeft++;}
+	if($counterLeft==1 && $counterRight==0){break;}
+	else if($input[$i]=='('){$counterRight++;}
+}
+if($counterRight!=$counterLeft){echo "<p>"."Что-то не так";}
+else {echo "<p>"."Все отлично";}
+?>
+</BODY>
+</HTML>
+
